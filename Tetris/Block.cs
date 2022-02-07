2e61cf7f0726a34bad548e5738a6a47e2727d2d1
TheKacho <abyssknightzero@gmail.com>
2022-02-07 13:36:04 -0500

    Add project files.

---
+++ Tetris/Block.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; } // start offset decides where the block spawns in the grid
+        protected abstract Position StartOffset { get; } // this also decides where the block spawns
+
+        public  abstract int Id { get; }
+
+        // this will store current rotation state and current offset
+        private int rotationState;
+        private Position offset;
+
+        public Block() // cpnstructor taht sets offset equal to start offset
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column); // factoring in current roation and offset
+        }
+
+        public IEnumerable<Position> TilePositions() // method loops over tile positions in current rotation state
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row +offset.Row, p.Column + offset.Column); //this adds the row offset and column offset
+
+            }
+        }
+
+        public void RotateCW() // rotates the piece 90 degrees/clockwise
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+        public void RotateCCW() // this rotates it counterclockwise
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+    }
+}
---
+++ Tetris/GameGrid.cs
@@ -0,0 +1,98 @@
+﻿namespace Tetris
+{
+    public class GameGrid
+    {
+        private readonly int [,] grid;
+
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int r, int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value; // with these in place, can use indexing directly on
+                                       // game grid object
+        }
+
+        public GameGrid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns];
+        }
+
+        // this will check if the given row/ and column is inside grid or not
+        public bool IsInside(int r, int c)
+        {
+            return r >= 0 && c >= 0 && r < Rows && c < Columns;
+                    // row must be greater than/equal 0 and less than number of rows
+                    // colums is the same thing, with less than number of columns
+        }
+
+        // this method checks if the cell is empty or not
+
+        public bool IsEmpty(int r, int c)
+        {
+            return IsInside(r, c) && grid[r, c] == 0;
+            // this means that it must be inside the grid and the value
+            // of that entry in array must be zero
+        }
+
+        // this method checks if any rwos are full
+        public bool IsRowFull(int r)
+        {
+            for(int c = 0; c < Columns; c++)
+            {
+                if (grid[r, c] == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+        //this checks if the row is empty
+        public bool IsRowEmpty(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                if (grid [r, c] != 0)
+                {
+                    return false;
+                }
+            }
+            return true; 
+                       
+        }
+
+        // anytime the rows are cleared, any non full rows are moved down
+        private void ClearRow(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r, c] = 0;
+            }
+        }
+
+        // this moves the rows down by a certain amount each time a full row is cleared
+        private void MoveRowDown(int r, int numRows)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r + numRows, c] = grid[r, c];
+                grid[r, c] = 0;
+
+            }
+        }
+
+        // this will clear any full rows
+        public int ClearFullRows()
+        {
+            int cleared = 0;
+
+        }
+
+    }
+}
